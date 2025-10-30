f194197dcc786d7631f4b003fcb7e59977da9464
Anto7304 <antonywanyaga2004@gmail.com>
2025-10-30 22:53:43 +0300

    modified

--- STUDENT MANAGEMENT TC.py
+++ STUDENT MANAGEMENT TC.py
@@ -25,7 +25,8 @@ def add_student():
     # combining data to one dictionary
     student = {"info": info, "course": course, "marks": marks}
     students.append(student)
-    print("{name} added successfully!!")
+    print(f"{name} added successfully!!")
+
 
 # function to display all students 
 def display_students():
@@ -33,7 +34,7 @@ def display_students():
     if not students:
         print("No students are available")
     else:
-        for i, student in enumarate(students, 1):
+        for i, student in enumerate(students, 1):
             name, age = student["info"]
             print(f"{i}. {name} {age} yrs) - {student["course"]}")
             for sub, marks in student["marks"].items():
@@ -56,43 +57,39 @@ def top_performer():
             top_avg = avg
             top_student = student
         
-    name. _ = top_student["info"]
-    print(f" TOP PERFORMER: {name} with average (top_avg:.2f)")
+    name, _ = top_student["info"]
+    print(f" TOP PERFORMER: {name} with average {top_avg:.2f}")
 
     # Tunataka tutrack unique courses: so hapa tutatumia sets
-    course = set()
-
-    '''
-     finally tunaunda menu ya ku allow the user a view students, 
-     add students ama pia kucheck top performer and finally ku exit
-     tutatumia while loop
-     '''
+courses = set()
 
 
-    while True:
-        # mennu 
-        print(" ----- STUDENT MANAGEMENT SYSTEM -----")
-        print("1. Add a Student")
-        print("2. View Students ")
-        print("3. Show Top Performer")
-        print("4. Exit")
 
-        # we fetch the input from the user for any of the choices atachagua
-        choice = input("Enter Choice: ")
+
+while True:
+    # mennu 
+    print(" ----- STUDENT MANAGEMENT SYSTEM -----")
+    print("1. Add a Student")
+    print("2. View Students ")
+    print("3. Show Top Performer")
+    print("4. Exit")
+
+    # we fetch the input from the user for any of the choices atachagua
+    choice = input("Enter Choice: ")
 # hizi ni call functions
-        if choice == "1":
-            add_student()
-            courses.add(students[-1]["course"])
-        elif choice == "2":
-            display_students()
-        elif choice == "3":
-            top_performer()
-        elif choice == "4":
-            print(f"\nUnique courses: {courses}")# hapa nili intergrate na AI
-            print("Goodbye.....exiting")
-            break
-        else: 
-            print("INVALID CHOICE!!!, try again.")
+    if choice == "1":
+        add_student()
+        courses.add(students[-1]["course"])
+    elif choice == "2":
+        display_students()
+    elif choice == "3":
+        top_performer()
+    elif choice == "4":
+        print(f"\nUnique courses: {courses}")# hapa nili intergrate na AI
+        print("Goodbye.....exiting")
+        break
+    else: 
+        print("INVALID CHOICE!!!, try again.")
 
 
 
